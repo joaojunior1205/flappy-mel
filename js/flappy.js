b188e25c66b60114a9ce6f9230923ccb64ce9c38
Joao Junior <joao.alves@nectarcrm.com.br>
2023-09-11 17:53:27 -0300

    touch click

--- js/flappy.js
+++ js/flappy.js
@@ -85,6 +85,14 @@ function Passaro(alturaJogo) {
     window.onkeydown = e => voando = true;
     window.onkeyup = e => voando = false;
 
+    // Adicione um ouvinte de evento para quando o usuário tocar na tela
+    window.addEventListener('touchstart', () => voando = true);
+
+    // Adicione um ouvinte de evento para quando o usuário parar de tocar na tela
+    window.addEventListener('touchend', () => voando = false);
+
+    window.onclick = e => voando = true;
+
     this.animar = () => {
         const novoY = this.getY() + (voando ? 8 : -5);
         const alturaMaxima = alturaJogo - this.elemento.clientWidth;
